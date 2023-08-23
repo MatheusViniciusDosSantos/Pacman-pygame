9ac2a35547f39fbe447adca62c8c5cc4f3ce07bd
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2023-08-22 22:01:46 -0300

    Adicionado sprites

--- pacman.py
+++ pacman.py
@@ -1,8 +1,13 @@
 from objeto import Objeto
 from constantes import PACMAN
+from sprites import PacmanSprites
 
 class Pacman(Objeto):
     
     def __init__(self) -> None:
         super().__init__()
         self.tipo = PACMAN
+        self.sprites = PacmanSprites(self)
+
+    def update(self, dt):	
+        self.sprites.update(dt)
